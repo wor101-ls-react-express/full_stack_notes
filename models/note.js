1718dcb1b41468831b5a40e60df1583ba09eaa95
Will <wor101@gmail.com>
2021-12-08 17:36:24 -0500

    functioning refactored note app

--- models/note.js
+++ models/note.js
@@ -1,40 +1,24 @@
-
-// not sure this is required here
-require('dotenv').config();
-
 const mongoose = require('mongoose');
-
-const url = process.env.MONGODB_URI
-
-console.log('connecting to', url)
-
-mongoose.connect(url)
-  .then(result => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
 
 const noteSchema = new mongoose.Schema({
   content: {
     type: String,
-    minLength: 5,
-    required: true
+    required: true,
+    minlength: 5
   },
   date: {
     type: Date,
-    required: true
+    required: true,
   },
   important: Boolean,
-})
+});
 
 noteSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
   }
-})
+});
 
-module.exports = mongoose.model('Note', noteSchema)
+module.exports = mongoose.model('Note', noteSchema);
