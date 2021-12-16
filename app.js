58251f8ef3c6bf0bd4fac92fcb27a35bc171f9e8
Will <wor101@gmail.com>
2021-12-16 15:20:24 -0500

    added E2E testing using Cypress

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const config = require('./utils/config');
 const express = require('express');
+require('express-async-errors');
 const app = express();
 const cors = require('cors');
 const notesRouter = require('./controllers/notes');
+const usersRouter = require('./controllers/users');
+const loginRouter = require('./controllers/login');
 const middleware = require('./utils/middleware');
 const logger = require('./utils/logger');
 const mongoose = require('mongoose');
@@ -23,6 +26,13 @@ app.use(express.json());
 app.use(middleware.requestLogger);
 
 app.use('/api/notes', notesRouter);
+app.use('/api/users', usersRouter);
+app.use('/api/login', loginRouter);
+
+if (process.env.NODE_ENV === 'test') {
+  const testingRouter = require('./controllers/testing')
+  app.use('/api/testing', testingRouter)
+}
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
